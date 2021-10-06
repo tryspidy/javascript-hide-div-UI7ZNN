ae956e00431324c9aa2a6e1a625bcdf41ee6dfd5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 16:58:38 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// javascript
+<script>
+	document.getElementById("id").style.display = "none";  //hide
+	document.getElementById("id").style.display = "block"; //show
+	document.getElementById("id").style.display = ""; 	   //show
+</script>
+
+// html
+<html>
+	<div id="id" style="display:none">
+    <div id="id" style="display:block">
+</html>
+
+// jquery
+<script>
+	$("#id").hide();      
+	$("#id").show();
+</script>
